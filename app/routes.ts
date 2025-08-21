996ede35919dfd6b46af87560c692f114040aa76
StrongProspect <brandon@strongprospect.us>
2025-08-20 21:59:45 -0400

    tweaked route setup

--- app/routes.ts
+++ app/routes.ts
@@ -8,19 +8,20 @@ import {
 export default [
   layout("components/shared/layout.tsx", [
     index("routes/public/home.tsx"),
+    route("/signup", "routes/public/signup.tsx"),
     route("/login", "routes/public/login.tsx"),
     route("/blog", "routes/public/blog.tsx"),
     route("/help", "routes/public/help.tsx"),
     route("/terms", "routes/public/terms.tsx"),
     route("/tools", "routes/public/tools.tsx"),
     route("/why", "routes/public/why.tsx"),
-    layout("components/shared/authenticated.tsx", [
+    layout("components/shared/authorized.tsx", [
       route("/dashboard", "routes/authorized/dashboard.tsx"),
       route("/gigs_tours", "routes/authorized/gigs_tours.tsx"),
       route("/money", "routes/authorized/money.tsx"),
       route("/promo", "routes/authorized/promo.tsx"),
-      route("/login", "routes/authorized/settings.tsx"),
-      route("/login", "routes/authorized/toolbox.tsx"),
+      route("/settings", "routes/authorized/settings.tsx"),
+      route("/toolbox", "routes/authorized/toolbox.tsx"),
     ]),
   ]),
 ] satisfies RouteConfig;
